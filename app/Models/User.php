862acee41865f1fc317fb5d0cf0c02f7f0a0a725
Lian Mafutra <lianmafutra@gmail.com>
2022-11-21 15:10:42 +0700

    change email to username login

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,8 +20,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
     ];
 
