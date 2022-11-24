040c94a5e701adb5d3f5baafe013134ad2e49036
Lian Mafutra <lianmafutra@gmail.com>
2022-11-24 14:00:15 +0700

    profil new

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -19,17 +19,22 @@
         <section class="content">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-3">
                         <div class="card card-primary card-outline">
                             <div class="card-body box-profile">
                                 <div class="text-center">
-
                                     <img class="profile-user-img img-fluid img-circle" src="{{ $user->foto_url }}"
                                         alt="User profile picture">
                                 </div>
                                 <h3 class="profile-username text-center">{{ $user->name }}</h3>
                                 <p class="text-muted text-center">{{ $user->roles->pluck('name')[0] }}</p>
-                                <p class="text-muted text-center">Last Login : </p>
+                                <div class="form-group row">
+                                    <div class="text-center col-sm-12">
+                                        <button class="btn_upload_foto btn btn-secondary btn-sm"> <i
+                                                class="fas fa-upload"></i> Ubah Foto</button>
+                                    </div>
+                                </div>
+                                {{-- <p class="text-muted text-center">Last Login : </p> --}}
                                 {{-- <ul class="list-group list-group-unbordered mb-3">
                                     <li class="list-group-item">
                                         <b>Followers</b> <a class="float-right">1,322</a>
@@ -45,33 +50,60 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-3">
-
-                        <div class="card card-primary card-outline">
-                            <div class="card-header">
-                                <h3 class="card-title">Setting</h3>
-                                <div class="card-tools">
-                                    <button type="button" class="btn btn-tool" data-card-widget="collapse"
-                                        title="Collapse">
-                                        <i class="fas fa-minus"></i>
-                                    </button>
-                                </div>
-                            
-                              
+                    <div class="col-md-9">
+                        <div class="card">
+                            <div class="card-header p-2">
+                                <ul class="nav nav-pills">
+                                    <li class="nav-item"><a class="nav-link active" href="#profile"
+                                            data-toggle="tab">Profile</a></li>
+                                    <li class="nav-item"><a class="nav-link " href="#settings"
+                                            data-toggle="tab">Settings</a></li>
+                                    <li class="nav-item"><a class="nav-link " href="#settings"
+                                            data-toggle="tab">Activity</a></li>
+                                </ul>
                             </div>
-                            <div class="card-body box-profile">
-                              <ul class="list-group list-group mb-3">
-                                 <li class="list-group-item">
-                                    <a href="#" class="pl-3 btn btn-primary btn-block text-left">  <i class="nav-icon fas fa-folder"></i>  Ubah Foto</a>
-                                 </li>
-                                 <li class="list-group-item">
-                                    <a href="#" class="pl-3 btn btn-primary btn-block text-left">  <i class="nav-icon fas fa-folder"></i>  Ubah Data Profile</a>
-                                 </li>
-                                 <li class="list-group-item">
-                                    <a href="#" class="pl-3 btn btn-primary btn-block text-left">  <i class="nav-icon fas fa-folder"></i>  Ubah Password</a>
-                                 </li>
-                             </ul>
-
+                            <div class="card-body">
+                                <div class="tab-content">
+                                    <div class="active tab-pane" id="profile">
+                                        <form class="form-horizontal">
+                                            <div class="form-group row">
+                                                <label for="inputName" class="col-sm-2 col-form-label">Nama</label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" class="form-control" id="nama"
+                                                        value="{{ $user->name }}" placeholder="Nama">
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="inputEmail" class="col-sm-2 col-form-label">Username</label>
+                                                <div class="col-sm-10">
+                                                    <input disabled type="email" class="form-control" id="username"
+                                                        placeholder="Username" value="{{ $user->username }}">
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="inputEmail" class="col-sm-2 col-form-label">Role</label>
+                                                <div class="col-sm-10">
+                                                    <input disabled type="text" class="form-control" id="role"
+                                                        placeholder="Username" value="{{ $user->roles->pluck('name')[0] }}">
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="inputEmail" class="col-sm-2 col-form-label">Nomor HP</label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" class="form-control" id="kontak"
+                                                        placeholder="Nomor Handphone" value="">
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <div class="offset-sm-2 col-sm-10">
+                                                    <button type="submit" class="btn btn-primary">Update Profile</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class=" tab-pane" id="activity">
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -79,4 +111,32 @@
             </div>
         </section>
     </div>
+    <div class="modal fade" id="modal_upload_foto">
+        <div class="modal-dialog modal-sm">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Upload Foto Profil</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                  
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                    <button type="submit" class="btn btn-primary">Lanjutkan</button>
+                </div>
+              
+            </div>
+            <!-- /.modal-content -->
+        </div>
+    </div>
 @endsection
+@push('js')
+    <script>
+        $(".btn_upload_foto").click(function() {
+           $('#modal_upload_foto').modal('show')
+        });
+    </script>
+@endpush
