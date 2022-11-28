7216d94cb2ac876405ff27209ddcdc2073ae24ab
Lian Mafutra <lianmafutra@gmail.com>
2022-11-28 12:26:29 +0700

    user navbar

--- app/Utils/uploadFile.php
+++ app/Utils/uploadFile.php
@@ -11,25 +11,25 @@ class uploadFile
    {
       try {
          $name_uniqe =  pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME) . '-' . now()->timestamp . '.' . $file->getClientOriginalExtension();
-        
+
          $tahun = Carbon::now()->format('Y');
          $bulan = Carbon::now()->format('m');
-         $custom_path = $tahun.'/'.$bulan.'/'.$folder;
-         $path = storage_path('app/public/'.$custom_path);
-   
+         $custom_path = $tahun . '/' . $bulan . '/' . $folder;
+         $path = storage_path('app/public/' . $custom_path);
+
          if (!File::isDirectory($path)) {
-             File::makeDirectory($path, 0777, true, true);
+            File::makeDirectory($path, 0777, true, true);
          }
 
-         $file->storeAs('public/'.$tahun.'/'.$bulan.'/'.$folder, $name_uniqe);
+         $file->storeAs('public/' . $tahun . '/' . $bulan . '/' . $folder, $name_uniqe);
+
          return collect([
             'nama' => $name_uniqe,
             'path' => $custom_path,
-        ]);
+         ]);
+         
       } catch (\Throwable $th) {
          return redirect()->back()->with('error', 'Gagal' . $th, 400)->send();
       }
    }
-
- 
 }
