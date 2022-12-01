944ab6ef978c4b0bb378a803d9103693f77e3d7c
Lian Mafutra <lianmafutra@gmail.com>
2022-12-01 12:09:44 +0700

    modal dialog histori pengajuan opd

---
+++ resources/views/pengajuan-opd/modal-histori.blade.php
@@ -0,0 +1,138 @@
+<style>
+    .timeline-footer {
+        padding: 0 0 5px 10px !important;
+    }
+
+    .timeline-footer .time {
+        font-size: 12px;
+        color: rgb(147, 147, 147) !important;
+    }
+</style>
+<div class="modal fade" id="modal_lihat_histori">
+    <div class="modal-dialog modal-md">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Histori Berkas</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <section class="content">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="timeline">
+                                    <div>
+                                        <i class="fas fa-file-import bg-blue"></i>
+                                        <div class="timeline-item">
+                                            {{-- <h3 class="timeline-header"><a href="#">Admin Diskominfo</a> Kirim Pengajuan Berkas Rekomendasi </h3> --}}
+                                            <div class="timeline-body">
+                                                <a href="#">Admin Diskominfo</a> Mengirim Pengajuan Berkas
+                                                Rekomendasi
+                                            </div>
+                                            <div class="dropdown-divider"></div>
+
+                                            <div class="timeline-footer">
+                                                <span class="time"><i class="fas fa-clock"></i> 12:05</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <i class="fas fa-sync-alt bg-yellow" style="color: white !important"></i>
+                                        <div class="timeline-item">
+                                            <div class="timeline-body">
+                                                <a href="#">Admin Inspektorat</a> Memproses Berkas
+                                            </div>
+                                           
+                                            <div class="dropdown-divider"></div>
+                                            <div class="timeline-footer">
+                                                <span class="time"><i class="fas fa-clock"></i> 12:05</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
+                                       <i class="fas fa-times bg-red" style="color: white !important"></i>
+                                       <div class="timeline-item">
+                                           <div class="timeline-body">
+                                               <a href="#">Admin Inspektorat</a> Menolak Berkas Pengajuan
+                                           </div>
+                                           <div class="dropdown-divider"></div>
+                                           <div class="timeline-body">
+                                               <span> "Mohon lengkapi berkas" </span>
+                                           </div>
+                                           <div class="dropdown-divider"></div>
+                                           <div class="timeline-footer">
+                                               <span class="time"><i class="fas fa-clock"></i> 12:05</span>
+                                           </div>
+                                       </div>
+                                   </div>
+                                    <div>
+                                        <i class="fas fa-paste bg-yellow" style="color: white !important"></i>
+                                        <div class="timeline-item">
+
+                                            <div class="timeline-body">
+                                                <a href="#">Kasubag administrasi umum dan keuangan</a>
+                                                Verifikasi Berkas
+                                            </div>
+                                            <div class="dropdown-divider"></div>
+                                            <div class="timeline-footer">
+                                                <span class="time"><i class="fas fa-clock"></i> 12:05</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <i class="fas fa-paste bg-yellow" style="color: white !important"></i>
+                                        <div class="timeline-item">
+
+                                            <div class="timeline-body">
+                                                <a href="#">Inspektur</a> Verifikasi Berkas
+                                            </div>
+                                            <div class="dropdown-divider"></div>
+                                            <div class="timeline-footer">
+                                                <span class="time"><i class="fas fa-clock"></i> 12:05</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <i class="fas fa-file-contract bg-blue"></i>
+                                        <div class="timeline-item">
+
+                                            <div class="timeline-body">
+                                                <a href="#">Admin Inspektorat</a> Menyiapkan Berkas
+                                            </div>
+                                            <div class="dropdown-divider"></div>
+                                            <div class="timeline-footer">
+                                                <span class="time"><i class="fas fa-clock"></i> 12:05</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <i class="far fa-check-circle bg-green"></i>
+                                        <div class="timeline-item">
+
+                                            <div class="timeline-body">
+                                                <a href="#">Berkas Telah Selesai</a>
+                                            </div>
+
+                                            <div class="dropdown-divider"></div>
+                                            <div class="timeline-footer">
+                                                <span class="time"><i class="fas fa-clock"></i> 12:05</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {{-- <div>
+                                        <i class="fas fa-clock bg-gray"></i>
+                                    </div> --}}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
